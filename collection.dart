0236ac7f0430c7ae7bab35dbba5377049fc643c5
Your GitHub Username <sumeyahassensumi@gmail.com>
2025-06-19 03:01:28 -0700

    collection complated

--- collection.dart
+++ collection.dart
@@ -1,8 +1,16 @@
 void list(){
-  List<String> projects =['derash','agelgel','parematec','smart supermarket','digitasercute'];
+  List<String> projects =['derash','agelgel','parematec','smart supermarket','digitasercute','mango'];
   for(var item in projects){
     print("projact names are $item");
   }
+   var fruit=['apple','banana','orange','mango'];
+   fruit.add('penapple');//to add a single item
+   print(fruit);
+   fruit.remove('mango');//to remove a single item
+   print(fruit);
+    fruit.addAll(['grape','kiwi','watermelon']);//to add multiple items
+   print(fruit);
+  
 }
 void set(){
   // set are not duplicated
@@ -10,10 +18,15 @@ void set(){
   // set are mutable
   // set are iterable
   // set are not indexed
-  Set<String> projects ={'derash','agelgel','parematec','smart supermarket','digitasercute'};
+  var fruit={'apple','banana','orange','mango','penapple'};
+  Set<String> projects ={'derash','agelgel','parematec','smart supermarket','digitasercute','mango'};
   for(var item in projects){
     print("projact names are $item");
   }
+  print(fruit.union(projects));//to combine two sets
+  print(fruit);
+  fruit.difference(projects);//to find the difference between two sets
+  print(fruit);
 }
 void map(){
   Map<String,String> projacts={'derash':"rahimet and sumeya",'agelgel':"rahimet and sumeya",'parema-tec':"birtukan,rahmate and sumeya",'smart supermarket':"rahimate and sumeya",'digitasercute':"biniyamand sumeya",'digitasercute':"biniyamand sumeya"};
